9df6ebb54c7e8cf4f9bcf9e8fef6f188253d8278
Jed Hunsaker <jed.hunsaker@gmail.com>
2013-03-07 14:24:54 -0800

    implement everything up to zoom window

--- desktop/js/app.js
+++ desktop/js/app.js
@@ -2,36 +2,80 @@
     'use strict';
 
     var App = Ember.Application.create(),
-        productImgPath = '/img/products/%@/medium/%@.jpg';
+        Color,
+        Price,
+        XS, S, M, L, XL,
+        productImgPath = '/img/products/%@/%@/%@.jpg';
     exports.App = App;
 
+    // Custom Objects
+    Color = Ember.Object.extend({
+        activeCameraAngle : 'angles.front',
 
-    // Data
+        catalogImg : function() {
+            return this.get( 'activeCameraAngle' ) === 'angles.front' ?
+                this.get( 'frontCatalogImg' ) :
+                this.get( 'backCatalogImg' );
+        }.property( 'activeCameraAngle' ),
 
-    var Event = Ember.Object.extend({
-        // productsBinding : Ember.Binding.oneWay('App.Data.Products')
+        frontCatalogImg : function() {
+            return productImgPath.fmt(
+                this.get( 'product' ).style, 'catalog', this.get( 'angles.front' ) );
+        }.property( 'product', 'angles.front' ),
+
+        backCatalogImg : function() {
+            return productImgPath.fmt(
+                this.get( 'product' ).style, 'catalog', this.get( 'angles.back' ) );
+        }.property( 'product', 'angles.back' ),
+
+        frontThumbImg : function() {
+            return productImgPath.fmt(
+                this.get( 'product' ).style, 'small', this.get( 'angles.front' ) );
+        }.property( 'product', 'angles.front' ),
+
+        backThumbImg : function() {
+            return productImgPath.fmt(
+                this.get( 'product' ).style, 'small', this.get( 'angles.back' ) );
+        }.property( 'product', 'angles.back' ),
+
+        materialIconImg : function() {
+            return '/img/products/%@/%@.jpg'.fmt(
+                this.get('product').style, this.get('name').replace(/ /g, '_').toLowerCase() );
+        }.property( 'product', 'name' ),
+
+        dashedName : function() {
+            return this.get('name').dasherize();
+        }.property( 'name' )
     });
 
-    var Color = Ember.Object.extend({
-        frontImgSrc : function() {
-            return productImgPath.fmt( this.get( 'product' ).style, this.get( 'front' ));
-        }.property( 'product', 'front' ),
-        backImgSrc : function() {
-            return productImgPath.fmt( this.get( 'product' ).style, this.get( 'back' ));
-        }.property( 'product', 'back' )
+    Price = Ember.Object.extend({
+
+        discount : function() {
+            return Math.round( 100 - this.get('sale')/this.get('retail')*100 );
+        }.property( 'sale', 'retail' )
     });
+
+    XS = Ember.Object.extend( {name : 'XS'} );
+    S = Ember.Object.extend( {name : 'S'} );
+    M = Ember.Object.extend( {name : 'M'} );
+    L = Ember.Object.extend( {name : 'L'} );
+    XL = Ember.Object.extend( {name : 'XL'} );
+
 
     App.Data = {
 
         Events : [
-            Event.create({
+            Ember.Object.create({
                 id : 137,
-                products : [256]
-            }), Event.create({
-                id : 138,
+                title : 'TART',
                 products : []
-            }), Event.create({
+            }), Ember.Object.create({
+                id : 138,
+                title : 'foo',
+                products : []
+            }), Ember.Object.create({
                 id : 139,
+                title : 'bar',
                 products : []
             })
         ],
@@ -41,91 +85,59 @@
                 id : 256,
                 title : 'Sea Breeze Villa/Faith Dress',
                 style : 'T1606',
-                options : [{
-                    color : 404,
-                    size : -2,
-                    quantity : {reserved : 0, available : 1},
-                    price : {list : 194, sale : 79}
-                }, {
-                    color : 405,
-                    size : -1,
-                    quantity : {reserved : 0, available : 1},
-                    price : {list : 194, sale : 79}
-                }, {
-                    color : 406,
-                    size : 0,
-                    quantity : {reserved : 0, available : 0},
-                    price : {list : 194, sale : 79}
-                }, {
-                    color : 407,
-                    size : 0,
-                    quantity : {reserved : 0, available : 0},
-                    price : {list : 194, sale : 79}
-                }]
-            }),
-            Ember.Object.create({
-                id : 257,
-                title : 'Sea Breeze Villa/Faith Dress',
-                style : 'T1606',
-                options : [{
-                    color : 404,
-                    size : -2,
-                    quantity : {reserved : 0, available : 1},
-                    price : {list : 194, sale : 79}
-                }, {
-                    color : 405,
-                    size : -1,
-                    quantity : {reserved : 0, available : 1},
-                    price : {list : 194, sale : 79}
-                }, {
-                    color : 406,
-                    size : 0,
-                    quantity : {reserved : 0, available : 0},
-                    price : {list : 194, sale : 79}
-                }]
+                colors : []
             })
         ],
 
         Colors : [
             Color.create({
-                id : 404,
+                id : 800,
                 name : 'BLACK 001MS',
-                front : 1592256,
-                back : 1592255
+                angles : {front : 1592256, back : 1592255},
+                price : Price.create( {sale : 79, retail : 194} ),
+                sizes : [
+                    XS.create( {inStock : 1, onHold : 0} ),
+                    S.create( {inStock : 1, onHold : 1} ),
+                    M.create( {inStock : 3, onHold : 0} ),
+                    L.create( {inStock : 0, onHold : 0} ),
+                    XL.create( {inStock: 0, onHold : 0} )
+                ]
             }), Color.create({
-                id : 405,
-                name : 'PALM 6248A',
-                front : 1592258,
-                back : 1592257
+                id : 801,
+                name : 'PALM 6248A 6248A',
+                angles : {front : 1592258, back : 1592257},
+                price : Price.create( {sale : 79, retail : 194} ),
+                sizes : [
+                    XS.create( {inStock : 1, onHold : 0} ),
+                    S.create( {inStock : 1, onHold : 1} ),
+                    M.create( {inStock : 3, onHold : 0} ),
+                    L.create( {inStock : 0, onHold : 0} ),
+                    XL.create( {inStock: 0, onHold : 0} )
+                ]
             }), Color.create({
-                id : 406,
-                name : 'RED 944MS',
-                front : 1592254,
-                back : 1592253
+                id : 802,
+                name : 'RED 944MS 944MS',
+                angles : {front : 1592254, back : 1592253},
+                price : Price.create( {sale : 79, retail : 194} ),
+                sizes : [
+                    XS.create( {inStock : 1, onHold : 1} ),
+                    S.create( {inStock : 12, onHold : 1} ),
+                    M.create( {inStock : 3, onHold : 0} ),
+                    L.create( {inStock : 0, onHold : 0} ),
+                    XL.create( {inStock: 0, onHold : 0} )
+                ]
             }), Color.create({
-                id : 407,
-                name : 'STRIPE 6231A',
-                front : 1592468,
-                back : 0
-            })
-        ],
-
-        Sizes : [
-            Ember.Object.create({
-                id : -2,
-                name : 'XS'
-            }), Ember.Object.create({
-                id : -1,
-                name : 'S'
-            }), Ember.Object.create({
-                id : 0,
-                name : 'M'
-            }), Ember.Object.create({
-                id : 1,
-                name : 'L'
-            }), Ember.Object.create({
-                id : 2,
-                name : 'XL'
+                id : 803,
+                name : 'STRIPE 6231A 6231A',
+                angles : {front : 1592468, back : 1592681},
+                price : Price.create( {sale : 79, retail : 194} ),
+                sizes : [
+                    XS.create( {inStock : 1, onHold : 0} ),
+                    S.create( {inStock : 1, onHold : 1} ),
+                    M.create( {inStock : 3, onHold : 0} ),
+                    L.create( {inStock : 0, onHold : 0} ),
+                    XL.create( {inStock: 0, onHold : 0} )
+                ]
             })
         ]
 
@@ -134,19 +146,18 @@
 
     // Routers
     App.Router.map(function() {
-        this.resource('events', function() {
-            this.resource('event', {path : ':event_id'}, function() {
-                this.resource('quick-look', {path : 'quick-look/:product_id'});
+        this.resource( 'events' );
+        this.resource( 'event', {path : '/event/:event_id'}, function() {
+            this.resource( 'quick-look', {path : 'quick-look/:product_id'}, function() {
+                this.resource( 'color', {path : 'color/:color_name'} );
             });
         });
-        this.resource('products', function() {
-            this.resource('product', {path : ':product_id'});
-        });
+        this.resource( 'product', {path : '/product/:product_id'});
     });
 
     App.IndexRoute = Ember.Route.extend({
         redirect : function() {
-            this.transitionTo('events');
+            this.transitionTo( 'events' );
         }
     });
 
@@ -157,35 +168,201 @@
     });
 
     App.EventRoute = Ember.Route.extend({
-        model : function( path ) {
-            var ev = App.Data.Events.findProperty( 'id', parseInt( path.event_id, 10 ) );
-
-            ev.products.forEach( function( productId, i ) {
-                var product = App.Data.Products.findProperty( 'id', productId );
-                this.products[i] = product;
-
-                product.options.forEach( function( option, j ) {
-                    option.color = App.Data.Colors.findProperty( 'id', option.color );
-                    option.color.product = this;
-                }, product );
-
-            }, ev);
-
-            return ev.products;
+        model : function( params ) {
+            return App.Data.Events.findProperty( 'id', parseInt( params.event_id, 10 ) );
         }
     });
 
-    App.ProductsRoute = Ember.Route.extend({
-        model : function() {
-            return App.Data.Products;
+    App.QuickLookRoute = App.ProductRoute = Ember.Route.extend({
+        model : function( params ) {
+            return App.Data.Products.findProperty( 'id', parseInt( params.product_id, 10 ) );
+        }
+    });
+
+    App.ColorRoute = Ember.Route.extend({
+        model : function( params ) {
+            return App.Data.Colors.findProperty( 'name', params.color_name.replace(/-/g, ' ').toUpperCase() );
+        },
+        serialize : function( model ) {
+            return {
+                color_name : model.get('name').dasherize()
+            };
         }
     });
 
 
     // Controllers
     App.EventsController = Ember.ArrayController.extend();
-    App.EventController = Ember.ArrayController.extend();
-    App.ProductsController = Ember.ArrayController.extend();
+
+    App.EventController = Ember.ObjectController.extend();
+
+    App.QuickLookController = Ember.ObjectController.extend({
+
+        closeModal : function() {
+            this.transitionToRoute( 'event' );
+        }
+    });
+
+    App.ColorController = Ember.ObjectController.extend({
+        selectedSize : null,
+        quantities : [],
+
+        selectPrevColor : function() {
+            this.relativeSelectColor( -1 );
+        },
+
+        selectNextColor : function() {
+            this.relativeSelectColor( 1 );
+        },
+
+        relativeSelectColor : function( incrementBy ) {
+            var colors = App.Data.Colors,
+                index = colors.indexOf( this.get( 'content' ) ) + incrementBy,
+                lastIndex = colors.length - 1;
+
+            this.setCameraAngle( 'angles.front' );
+            this.setSize( null );
+            this.transitionToRoute( 'color',
+                colors[ index < 0 ? lastIndex : index > lastIndex ? 0 : index ]);
+        },
+
+        setCameraAngle : function( angle ) {
+            this.set( 'activeCameraAngle', angle );
+        },
+
+        setSize : function( size ) {
+            this.set( 'selectedSize', size );
+            this.setAvailableQuantity( size && size.inStock - size.onHold || 0 );
+        },
+
+        setAvailableQuantity : function( availableQuantity ) {
+            var max = availableQuantity <= 6 ? availableQuantity : 6;
+
+            for ( var i = 1, quantities = []; i <= max; i++ ) {
+                quantities.push( i );
+            }
+
+            this.set( 'quantities', quantities );
+        }
+    });
+
+    // App.ProductsController = Ember.ArrayController.extend({
+    //     selectedColor : null,
+
+    //     selectColor : function( color ) {
+    //         this.set( 'selectedColor', color );
+    //     }
+    // });
+
+
+    App.Views = {
+
+        ZoomWrapper : Ember.View.extend({
+            $foo : 12,
+            eventManager : Ember.Object.create({
+                mouseEnter : function( e, view ) {
+                    var $view = view.$();
+                    this.$children = $view.children();
+                    this.offset = $view.offset();
+                },
+                mouseMove : function( e, view ) {
+                    var $children = this.$children,
+                        x = e.pageX - this.offset.left,
+                        y = e.pageY - this.offset.top,
+                        left = x - 35,
+                        top = y - 62;
+
+                    left = left < 1 ? 1 : left > 206 ? 206 : left;
+                    top = top < 1 ? 1 : top > 291 ? 291 : top;
+
+                    $children.filter( '.window' ).css( {left : left, top : top } );
+                    $children.filter( '.top' ).css( 'height' , top );
+                    $children.filter( '.left' ).css( {top : top, width : left} );
+                    $children.filter( '.right' ).css( {top : top, width : 207 - left} );
+                    $children.filter( '.bottom' ).css( 'height' , 292 - top );
+                    $children.show();
+                },
+                mouseLeave : function( e, view ) {
+                    view.$().children().hide();
+                }
+            })
+        }),
+
+        ThumbBorder : Ember.View.extend({
+            classNameBindings : ['isActive:active'],
+
+            isActive : function() {
+                return this.get( 'controller.activeCameraAngle' ) === this.get( 'content' );
+            }.property( 'controller.activeCameraAngle', 'content' ),
+
+            eventManager : Ember.Object.create({
+                mouseEnter : function( e, view ) {
+                    view.get( 'controller' ).send( 'setCameraAngle', view.get( 'content' ));
+                },
+                click : function( e, view ) {
+                    view.get( 'controller' ).send( 'setCameraAngle', view.get( 'content' ));
+                }
+            })
+
+        }),
+
+        ColorButton : Ember.View.extend({
+            tagName : 'span',
+            classNameBindings : [':color', 'isSelected:selected'],
+
+            isSelected : function() {
+                return this.get( 'controller.content' ) === this.get( 'content' );
+            }.property( 'controller.content', 'content' ),
+
+            click : function( e ) {
+                this.get( 'controller' ).send( 'setSize', null );
+            }
+        }),
+
+        SizeButton : Ember.View.extend({
+            tagName : 'dd',
+
+            isSelected : function() {
+                return this.get( 'controller.selectedSize' ) === this.get( 'content' );
+            }.property( 'controller.selectedSize', 'content' ),
+
+            click : function( e ) {
+                this.get( 'controller' ).send( 'setSize', this.get( 'content' ) );
+            }
+
+        })
+
+    };
+
+
+    // View Helpers
+    Ember.Handlebars.registerBoundHelper('money', function( dollars ) {
+        return dollars.toFixed(2);
+    });
+
+
+    App.prototype = {
+
+        constructor : function() {
+            this.bindModels();
+        },
+
+        bindModels : function() {
+            var event = App.Data.Events[0];
+            App.Data.Products.forEach( function( product ) {
+                product.event = event;
+                event.products.push( product );
+
+                App.Data.Colors.forEach( function( color ) {
+                    color.product = product;
+                    product.colors.push( color );
+                });
+            });
+        }
+
+    };
+
+    App.prototype.constructor();
 
 
 }( window ));
